87fe50e385a388143c0d0349ef716dccff2bc9b1
fra533 <75169012+fra533@users.noreply.github.com>
2025-03-13 16:05:13 +0100

    aggiunti test

--- OC_queries.py
+++ OC_queries.py
@@ -1,9 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 OPENCITATION_META_API_URL = "https://opencitations.net/meta/api/v1/metadata/"
+selected_papers= "C:\Users\franc\OneDrive - Alma Mater Studiorum Università di Bologna\Desktop\IRCDL\data\IRCDL - paper resource selection  - Selected resources.csv"
 
 
-def doi_OpenCitation_info(doi):
+def doi_OpenCitation_META_info(doi):
+    """Get info for DOI in OpenCitation Meta."""
 
-    
     return True 
 
+def doi_OpenCitation_index_info(doi):
+    """Get info for DOI in OpenCitation Index."""
+
+    return True
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
